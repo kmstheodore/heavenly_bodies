057697dc4e81b68352db1fab30df01fd26ff7aa2
Theodore Simpson <kmstheodore@gmail.com>
2022-08-29 16:00:54 +0100

    ife fixed :D

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_131451) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_29_145629) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,8 +18,22 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_131451) do
     t.string "name"
     t.float "price"
     t.text "desc"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_planets_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "planets", "users"
 end
