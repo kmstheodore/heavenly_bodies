3fd98d87dcd8825933c72112eab1a252b0fb4914
Paulo Gomes <pjgomes85@hotmail.com>
2022-08-29 17:20:44 +0100

    Log-in/Sign-up pages working

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,10 @@ class User < ApplicationRecord
   validates :name, presence: true
 
 
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
+
