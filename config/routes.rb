575a03aaaf7f7d76d25f22350807c6409b97a32a
Paulo Gomes <pjgomes85@hotmail.com>
2022-08-29 14:33:39 +0100

    create Integrate User

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  devise_for :users
+
+
+
+  get '/signin', to: "sessions#new"
+  post '/signin', to: "sessions#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
