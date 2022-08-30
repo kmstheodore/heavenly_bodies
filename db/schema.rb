d8523466f2db9e1f48b5137f97795827144805fb
Paulo Gomes <pjgomes85@hotmail.com>
2022-08-30 16:01:46 +0100

    cleqn schma

--- db/schema.rb
+++ db/schema.rb
@@ -64,7 +64,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_124458) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
