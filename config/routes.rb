ea83b35c8cc58a25e28236ada19c589030b97211
Jean Charles Piette <piette.jc@me.com>
2022-08-30 16:12:36 +0100

    fix welcome home page issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'welcome#home'
-  get '/auth/facebook/callback' => 'sessions#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -64,6 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_124458) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -72,6 +73,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_124458) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
