2fd624ad8c091547cffd713419cea390048a91d0
Jean Charles Piette <piette.jc@me.com>
2022-08-30 14:14:28 +0100

    force master

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, uniqueness: true, presence: true
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
