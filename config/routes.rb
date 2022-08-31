10e1658c469b0fef937d7094cb8bdc3201dc26e0
Theodore Simpson <kmstheodore@gmail.com>
2022-08-31 12:05:42 +0100

    olivia carried

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'pages#home'
+  get 'planets/:id', to: 'planets#show', as: :planet
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
