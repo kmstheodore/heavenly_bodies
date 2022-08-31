865b2a058187575d7f408f6b9ae85b0a433ad5ec
Jean Charles Piette <piette.jc@me.com>
2022-08-31 12:54:49 +0100

    booking fix

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,7 +3,7 @@ class BookingsController < ApplicationController
 
   # GET /bookinges or /bookinges.json
   def index
-    @bookings = Booking.where(user_id: current_user.id)
+    @bookinges = booking.where(user_id: current_user.id)
   end
 
   # GET /bookinges/1 or /bookinges/1.json
@@ -12,7 +12,7 @@ class BookingsController < ApplicationController
 
   # GET /bookinges/new
   def new
-    @booking = Booking.new
+    @booking = booking.new
   end
 
   # GET /bookinges/1/edit
@@ -21,14 +21,14 @@ class BookingsController < ApplicationController
 
   # POST /bookinges or /bookinges.json
   def create
-    @booking = Booking.new(booking_params)
+    @booking = booking.new(booking_params)
 
     respond_to do |format|
       if @booking.save
         format.html { redirect_to booking_url(@booking), notice: "booking was successfully created." }
         format.json { render :show, status: :created, location: @booking }
       else
-        format.html { redirect_to root_path, status: :unprocessable_entity }
+        format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @booking.errors, status: :unprocessable_entity }
       end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :planets, only: [:show] do
+    resources :bookings, only: [:create]
+  end
+
   root to: 'pages#home'
-  post '/booking-new', to: 'bookings#create'
-  get 'planets/:id', to: 'planets#show', as: :planet
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
