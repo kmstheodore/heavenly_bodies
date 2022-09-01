00d13c2bfc7582ae0ddf1359873ebc308163d3d3
Jean Charles Piette <piette.jc@me.com>
2022-09-01 11:23:20 +0100

    not allow to book a planet at an existing date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :planet
+
+  validates :planet_id, uniqueness: { scope: :start_date, message: 'This planet is already booked at this date !' }
 end
