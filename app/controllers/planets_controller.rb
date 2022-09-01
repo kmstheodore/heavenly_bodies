5284b39e12ce0a56f423d526e006036a9375f074
Paulo Gomes <pjgomes85@hotmail.com>
2022-09-01 14:30:30 +0100

    add planets

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -7,4 +7,29 @@ class PlanetsController < ApplicationController
     @planet = Planet.find(params[:id])
     @booking = Booking.new
   end
+
+  def create
+    @planet = Planet.create(planet_params)
+    @planet.user_id = current_user.id
+    if @planet.save
+      redirect_to planets_path(@planet)
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    @planet = Planet.new
+  end
+
+  def destroy
+    @planet.destroy
+    redirect_to planets_path(@planet.list), status: :see_other
+  end
+
+  private
+
+  def planet_params
+    params.require(:planet).permit(:name, :price, :desc, :photo)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   resources :bookings
 
-  resources :planets, only: [:show] do
+
+  resources :planets, only: [:show, :new, :create] do
     resources :bookings, only: [:create]
   end
 
   root to: 'pages#home'
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
